4033def79cef58c586476294d260bcc5aa6bd6b7
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2025-08-08 22:57:02 -0300

    Tentando implementar arcos, bug ao fornecer start e finish angle

--- basico2.js
+++ basico2.js
@@ -1,5 +1,5 @@
 class Ativo{
-  constructor(x, y, name, price, amount, data_com = "", data_pagamento = ""){
+  constructor(x, y, name, price, amount, data_com = "", data_pagamento = "", start_angle = 0){
     this.x = x;
     this.y = y;
 
@@ -11,8 +11,15 @@ class Ativo{
     
     this.informacoes = [this.name, this.price, this.amount];
     this.desloc = 5;
+
+    this.start_angle = start_angle; //start angle for the arc
   }
   
+  updateFinishAngle(valor_total){
+    this.valor_total = valor_total;
+    this.finish_angle = (((this.price * this.amount) / (100 * this.valor_total)) * 2 * Math.PI) + this.start_angle; // calculate the end angle based on the percentage
+  }
+
   displayInfo(){
     fill(255,255,255);
     rect(this.x - 20, this.y - 40, this.informacoes.length * 50, 150);
@@ -41,6 +48,11 @@ class Ativo{
     fill(0,0,0);
     text("Voltar", x_voltar + 20, y_voltar + 50);
   }
+
+  displayArc(cor){
+    fill(cor);
+    arc(600, 400, 200, 200, this.start_angle, this.finish_angle, PIE);
+  }
 }
 
 //creating objects
@@ -68,6 +80,10 @@ var y_voltar = 1000;
 var largura_voltar = 100;
 var altura_voltar = 100;
 
+var valor_total = 0;
+
+var start_angle = 0;
+
 function setup() {
   createCanvas(1700, 1300);
   titulo = dados.titulo;
@@ -77,14 +93,43 @@ function setup() {
     var y = 300 + Math.floor(i / 4) * 300; //distribute objects in a grid
 
     var ativo = ativos[i];
-    console.log(ativo);
-    objetos.push(new Ativo(x, y, ativo.nome, ativo.preco_atual, ativo.quantidade, ativo.data_com, ativo.data_pagamento));
+    
+    valor_total += ativo.preco_atual * ativo.quantidade;
+
+    objetos.push(new Ativo(x, y, ativo.nome, ativo.preco_atual, ativo.quantidade, ativo.data_com, ativo.data_pagamento, start_angle));
     posicoes_x.push(x);
     posicoes_y.push(y);
   }
 
-  console.log(objetos);
+  for(var i = 0; i < objetos.length; i++){
+    var ativo = objetos[i];
+    if (i == 0){
+      ativo.updateFinishAngle(valor_total); // calculate the finish angle for each object
+      var new_start_angle = ativo.finish_angle; // update the angle for the next arc
+    }
+    else{
+      ativo.start_angle = new_start_angle;
+      ativo.updateFinishAngle(valor_total); // calculate the finish angle for each object
+      new_start_angle = ativo[i].finish_angle; // update the angle for the next arc
+    }
+
+    console.log(ativo);
+    console.log(ativo.valor_total);
+    console.log(ativo.start_angle);
+    console.log(ativo.finish_angle);
+  }
+
+  //console.log(objetos);
+  //console.log(valor_total);
 }
+
+var cor1 = getRandomRgbColor();
+var cor2 = getRandomRgbColor();
+var cor3 = getRandomRgbColor();
+var cor4 = getRandomRgbColor();
+var cor5 = getRandomRgbColor();
+
+var cores = [cor1, cor2, cor3, cor4, cor5];
 
 function draw() {    
   background('rgba(220, 255, 230, 1)');
@@ -96,6 +141,7 @@ function draw() {
     text("Clique em um ativo para ver mais informações", 50, 150);
     for(var i = 0; i < objetos.length; i++){
       objetos[i].displayInfo();
+      objetos[i].displayArc(cores[i % cores.length]);
     }
     text(mouseX + ", " + mouseY, 1500, 50);
   }
@@ -128,4 +174,11 @@ function mouseClicked(){ //navigate through screens
         tela = i; //go to screen i
     } 
   }
+}
+
+function getRandomRgbColor() {
+  const r = Math.floor(Math.random() * 256); // Random number for Red (0-255)
+  const g = Math.floor(Math.random() * 256); // Random number for Green (0-255)
+  const b = Math.floor(Math.random() * 256); // Random number for Blue (0-255)
+  return `rgb(${r},${g},${b})`; // Returns the color string in RGB format
 }
